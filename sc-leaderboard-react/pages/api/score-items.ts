7f63a77bbcdb93d8071eabddc509d59a199dab2c
Menno Fesevur <menno@zkr.nl>
2024-08-21 18:34:29 +0200

    Add better score-items api

--- sc-leaderboard-react/pages/api/score-items.ts
+++ sc-leaderboard-react/pages/api/score-items.ts
@@ -1,28 +1,24 @@
 import client from "../../lib/mongodb";
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === "GET") { 
-        try {
-            const db = client.db("Summercamp2025");
-            const points = await db
-                .collection("Points")
-                .find({})
-                .toArray();
-            res.json(points);
-        } catch (e) {
-            console.error(e);
-        }
+const a = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const db = client.db("Summercamp2025");
+      const points = await db.collection("Points").find({}).toArray();
+      res.json(points);
+    } catch (e) {
+      console.error(e);
     }
-    else if (req.method === "POST") {
-        try {
-            const db = client.db("Summercamp2025");
-            const points = await db
-                .collection("Points")
-                .insertOne(req.body);
-            res.json(points);
-        } catch (e) {
-            console.error(e);
-        }
-    }   
-}
+  } else if (req.method === "POST") {
+    try {
+      const db = client.db("Summercamp2025");
+      const points = await db.collection("Points").insertOne(req.body);
+      res.json(points);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+
+export default a;
